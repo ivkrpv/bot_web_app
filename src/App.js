d7d8b7f81e89a3f727d88e626b4215f7946d388f
Ivan Karpov <>
2022-04-19 22:05:08 +0200

    add leaflet and tailwind

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import logo from "./logo.svg";
-import "./App.css";
+import "leaflet/dist/leaflet.css";
+import { MapContainer, TileLayer, CircleMarker, Popup } from "react-leaflet";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Bot</p>
-        <p>{window.Telegram.WebApp.colorScheme}</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-screen w-full text-red-400 bg-slate-600">
+      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <CircleMarker center={[51.505, -0.09]}>
+          <Popup>
+            Your Telegram uses {window.Telegram.WebApp.colorScheme} theme.
+          </Popup>
+        </CircleMarker>
+      </MapContainer>
     </div>
   );
 }
